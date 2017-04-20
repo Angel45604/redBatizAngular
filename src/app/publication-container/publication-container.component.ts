02a229ece29470cccac92f391fc573e158504a34
Angel Marcos Montes <angel.mauricio.marcos@live.com>
2017-04-19 21:13:54 -0500

    DB Connection

--- src/app/publication-container/publication-container.component.ts
+++ src/app/publication-container/publication-container.component.ts
@@ -5,5 +5,9 @@
     styleUrls: ['publication-container.component.css']
 })
 export class PublicationContainer{
-    registers: any[] = [{ publicator: 'Pablo', date: '08/12/2010', text: 'Hola' }, { publicator: 'Felisa', date: '018/02/2000', text: 'Como' }, { publicator: 'Margarito', date: '28/04/2017', text: 'Estas?' }];
+    registers: any[] = [
+        { publicator: 'Pablo', date: '08/12/2010', text: 'Hola' }, 
+        { publicator: 'Felisa', date: '018/02/2000', text: 'Como' }, 
+        { publicator: 'Margarito', date: '28/04/2017', text: 'Estas?' }
+        ];
 }
