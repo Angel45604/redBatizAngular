6390b7d1e996bea656292339788ac3ea0ca18ccb
Angel <angel.mauricio.marcos@live.com>
2017-05-14 22:58:05 -0500

    :v

--- src/backend/app.js
+++ src/backend/app.js
@@ -11,18 +11,20 @@ const PORT = 3000;
 var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var Tasks=require('./router/routes/Tasks');
+
 app.use(cors());
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
-app.use(cors());
 app.use(cookieParser());
+
 app.use((req, res, next) => {
   res.header('Content-Type', 'application/json');
   next();
 });
 
 router(app, db);
+
 app.use('/', router);
 app.use('/Tasks',Tasks);
 //drop and resync with { force: true }
