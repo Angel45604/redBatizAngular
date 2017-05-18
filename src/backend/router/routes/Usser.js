99de20c96707cc153362a723cbbeb5093718e255
filiberto bueno rodriguez <filiberto bueno rodriguez>
2017-05-17 22:21:27 -0500

    los 3 selects para probarlos

--- src/backend/router/routes/Usser.js
+++ src/backend/router/routes/Usser.js
@@ -19,6 +19,26 @@ module.exports = (app, db) => {
   });*/
 
 //Obtener todos los grupos de un profesor
+app.get('/Usuarios/:maestroname/:maestroap/:maestroam', (req, res) => {
+     const maestroname= req.params.maestroname;
+     const maestroap= req.params.maestroap;
+     const maestroam= req.params.maestroam;
+     db.Usuarios.find({
+      where: { 
+        maestroname:maestroname,
+        maestroam:maestroam,
+        maestroap:maestroap
+
+      },
+      attributes: [
+        [sequelize.fn('DISTINTIC', sequelize.col('grupo'))]]
+    })
+      .then(Usuarios=> {
+        res.json(Usuarios);
+      });
+  });
+
+//obtener todos los alumnos del grupo de un profesor
 app.get('/Usuarios/:group/:maestroname/:maestroap/:maestroam', (req, res) => {
     const group= req.params.group;
      const maestroname= req.params.maestroname;
@@ -32,8 +52,32 @@ app.get('/Usuarios/:group/:maestroname/:maestroap/:maestroam', (req, res) => {
         maestroap:maestroap
 
       },
+      attributes: [ 'alumnoname','alumnoap','alumnoam','grupo']
+    })
+      .then(Usuarios=> {
+        res.json(Usuarios);
+      });
+  });
+
+//Obtener todos los profesores de un jefe de academia
+app.get('/Usuarios/:group/:jefeacademianame/:jefeacademiaap/:jefeacademiaam', (req, res) => {
+      const group= req.params.group;    
+     const jefeacademianame= req.params.jefeacademianame;
+     const jefeacademiaap= req.params.jefeacademiaap;
+     const jefeacademiaam= req.params.jefeacademiaam;
+     db.Usuarios.find({
+      where: { 
+        group:group,
+        jefeacademianame:jefeacademianame,
+        jefeacademiaam:jefeacademiaam,
+        jefeacademiaap:jefeacademiaap
+
+      },
       attributes: [
-        [sequelize.fn('DISTINTIC', sequelize.col('grupo'))]]
+        [sequelize.fn('DISTINTIC', sequelize.col('maestroname'))],
+        [sequelize.fn('DISTINTIC', sequelize.col('maestroap'))],
+        [sequelize.fn('DISTINTIC', sequelize.col('maestroam'))]
+        ]
     })
       .then(Usuarios=> {
         res.json(Usuarios);
